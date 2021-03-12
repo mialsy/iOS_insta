3ffcc01a1b510911cd36977191782a7d92c494f8
Chuxi Wang <wangchuxi94@gmail.com>
2021-03-12 11:47:37 -0800

    add choosing photo

---
+++ insta/CameraViewController.swift
@@ -0,0 +1,60 @@
+//
+//  CameraViewController.swift
+//  insta
+//
+//  Created by Mialsy on 3/12/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+    }
+    
+    @IBAction func onImageViewTap(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera)
+        {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaleddImage = image.af.imageScaled(to: size)
+        
+        imageView.image = scaleddImage
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
