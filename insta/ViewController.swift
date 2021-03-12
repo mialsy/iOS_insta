2c7153318f22e4ef82a159842eb9c5f5c5cbaf3a
Chuxi Wang <wangchuxi94@gmail.com>
2021-03-11 20:49:47 -0800

    Initial Commit

---
+++ insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  insta
+//
+//  Created by Mialsy on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
